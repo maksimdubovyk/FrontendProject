6cf79190875dd68235aac02cf39387a0df1fd3fd
Maksim Dubovyk <maksimdubovyk2004@gmail.com>
2024-11-01 19:09:06 +0200

    separation web page on parts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import '../styles/main.css';
+import CodeEditor from './CodeEditor';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='code-editor'>
+            <CodeEditor />
+        </div>
+        <div className='instrument-helpers'>
+            <div className='code-quality'>
+                {/* Аналіз якості  */}
+            </div>
+            <div className='code-from-ai'>
+                {/* Код від ШІ  */}
+            </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CodeEditor extends Component {
+  render() {
+    return (
+      <>
+        <p>Hello World</p>
+      </>
+    );
+  }
+}
+
+export default CodeEditor;
