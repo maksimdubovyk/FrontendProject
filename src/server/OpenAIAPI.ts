87a3a5cd0547e328ef37c60244a7117573da794e
Maksim Dubovyk <maksimdubovyk2004@gmail.com>
2024-11-09 19:05:54 +0200

    improve openAIAPI

--- src/server/OpenAIAPI.ts
+++ src/server/OpenAIAPI.ts
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import { OpenAIModels } from "./OpenAIModels";
 import { ProgrammingLanguage } from "../scripts/ProgrammingLanguage";
+import { ChatCompletionMessageParam } from "openai/resources";
 
 export class OpenAIAPI {
     private openai: OpenAI;
@@ -18,21 +19,32 @@ export class OpenAIAPI {
 
     public async analyzeCode(code: string): Promise<string> {
         const prompt = `Analyze the code and assess its clarity, categorizing issues as soft, medium, or hard. Provide only the quality assessment without code, shortening the text of the assessment to state the main problem without explanation:\n\n${code}`;
-        return await this.sendRequest(prompt);
+        const messages: ChatCompletionMessageParam[] = [
+            { role: "user", content: "" },
+            { role: "user", content: prompt },
+        ];
+        return await this.sendRequest(messages);
     }
+
     public async improveCode(
         code: string,
         language: ProgrammingLanguage,
     ): Promise<string> {
         const prompt = `Rewrite this ${language} code to make it high-quality, understandable, efficient, and compliant with the coding standards for the specified programming language, following the principles of readability, maintainability, and optimality. Generate only code without explanations:\n\n${code}`;
-        return await this.sendRequest(prompt);
+        const messages: ChatCompletionMessageParam[] = [
+            { role: "user", content: "" },
+            { role: "user", content: prompt },
+        ];
+        return await this.sendRequest(messages);
     }
 
-    private async sendRequest(prompt: string): Promise<string> {
+    private async sendRequest(
+        messages: ChatCompletionMessageParam[],
+    ): Promise<string> {
         try {
             const completion = await this.openai.chat.completions.create({
                 model: OpenAIModels.GPT_4O_MINI,
-                messages: [{ role: "user", content: prompt }],
+                messages,
             });
             return completion.choices[0].message.content.trim();
         } catch (error) {
